014508a1053224e867af7e2b677f000d858cf950
ramontrndd <134494678+ramontrndd@users.noreply.github.com>
2024-12-13 19:14:57 -0300

    feat: product-ordemItem on-to-many association

--- src/main/java/com/projectwebservice/webservice/entities/Product.java
+++ src/main/java/com/projectwebservice/webservice/entities/Product.java
@@ -1,5 +1,6 @@
 package com.projectwebservice.webservice.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serial;
@@ -26,8 +27,12 @@ public class Product implements Serializable {
             inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
 
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
+
     public Product() {
     }
+
 
     public Product(Long id, String name, String description, Double price, String imgUrl) {
         this.id = id;
@@ -79,6 +84,15 @@ public class Product implements Serializable {
 
     public Set<Category> getCategories() {
         return categories;
+    }
+
+    @JsonIgnore
+    public Set<Order> getOrders() {
+        Set<Order> set = new HashSet<>();
+        for (OrderItem x : items) {
+            set.add(x.getOrder());
+        }
+        return set;
     }
 
 
