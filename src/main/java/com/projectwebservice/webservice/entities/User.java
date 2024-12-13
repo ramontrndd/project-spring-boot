431fd8304e2a8b2894865b58c6577ea7cc9acf6d
ramontrndd <134494678+ramontrndd@users.noreply.github.com>
2024-12-13 01:51:51 -0300

    feat: user entity and resource

---
+++ src/main/java/com/projectwebservice/webservice/entities/User.java
@@ -0,0 +1,79 @@
+package com.projectwebservice.webservice.entities;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+public class User implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+    private long id;
+    private String name;
+    private String email;
+    private String password;
+    private String phone;
+
+    public User() {
+        super();
+    }
+
+    public User(long id, String name, String email, String password, String phone) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+        return id == user.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Long.hashCode(id);
+    }
+}
