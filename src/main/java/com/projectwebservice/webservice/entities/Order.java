e90ca026fcca2e2a7954722d47210f45bf6402a0
ramontrndd <134494678+ramontrndd@users.noreply.github.com>
2024-12-13 19:56:37 -0300

    feat: subtotal & totalmethods implement

--- src/main/java/com/projectwebservice/webservice/entities/Order.java
+++ src/main/java/com/projectwebservice/webservice/entities/Order.java
@@ -29,6 +29,9 @@ public class Order implements Serializable {
     @OneToMany(mappedBy = "id.order")
     private Set<OrderItem> items = new HashSet<>();
 
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
+
     public Order() {
     }
 
@@ -73,8 +76,24 @@ public class Order implements Serializable {
         }
     }
 
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
+    }
+
     public Set<OrderItem> getItems() {
         return items;
+    }
+
+    public Double getTotal() {
+        double sum = 0.0;
+        for (OrderItem x : items) {
+            sum += x.getSubTotal();
+        }
+        return sum;
     }
 
     @Override
