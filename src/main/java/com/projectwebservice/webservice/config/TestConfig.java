619489ac241b5ab4a9a8802c64cd3b4235382cea
ramontrndd <134494678+ramontrndd@users.noreply.github.com>
2024-12-13 15:05:07 -0300

    feat: add orderstatus enum

--- src/main/java/com/projectwebservice/webservice/config/TestConfig.java
+++ src/main/java/com/projectwebservice/webservice/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.projectwebservice.webservice.config;
 
 import com.projectwebservice.webservice.entities.Order;
 import com.projectwebservice.webservice.entities.User;
+import com.projectwebservice.webservice.entities.enums.OrderStatus;
 import com.projectwebservice.webservice.repositories.OrderRepository;
 import com.projectwebservice.webservice.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,9 +28,9 @@ public class TestConfig implements CommandLineRunner {
 
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
-        Order order1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-        Order order2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-        Order order3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+        Order order1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1, OrderStatus.PAID);
+        Order order2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2, OrderStatus.WAITING_PAYMENT);
+        Order order3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1, OrderStatus.CANCELED);
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(order1, order2, order3));
